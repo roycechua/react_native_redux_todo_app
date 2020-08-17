45157da1d54644e034c1fa30ab32cc095ffb52e3
Royce Chua <royce236@gmail.com>
2020-08-17 20:58:37 +0800

    Add reducer and actionTypes

--- src/redux/reducers/todos.js
+++ src/redux/reducers/todos.js
@@ -1,23 +1,52 @@
-import { ADD_TODO } from "../actionTypes";
+import {ADD_TODO, TOGGLE_TODO, DELETE_TODO, EDIT_TODO} from '../actionTypes';
 
-let todoID = 1
+let todoID = 1;
 
 const initialState = {
-  todos_list: []
+  todos_list: [],
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case ADD_TODO: {
-      const { task } = action.payload
-      // console.log(action.payload)
-      // console.log(todoID)
-      // console.log(id)
-      // console.log(task)
+      const {task} = action.payload;
       return {
         ...state,
-        // allTodoIds: [...state, nextTodoId],
-        todos_list: [...state.todos_list, {id: ++todoID, task: task, isDone: false}]
+        todos_list: [
+          ...state.todos_list,
+          {id: ++todoID, task: task, isDone: false},
+        ],
+      };
+    }
+    case TOGGLE_TODO: {
+      const {id} = action.payload;
+      const updatedItem = state.todos_list.find((element) => element.id === id);
+      updatedItem.isDone = !updatedItem.isDone;
+
+      return {
+        ...state,
+        todos_list: [
+          ...state.todos_list.filter((element) => element.id != id),
+          updatedItem,
+        ],
+      };
+    }
+    case DELETE_TODO: {
+      const {id} = action.payload;
+
+      return {
+        ...state,
+        todos_list: [...state.todos_list.filter((element) => element.id != id)],
+      };
+    }
+    case EDIT_TODO: {
+      const { id, task, isDone } = action.payload;
+      return {
+        ...state,
+        todos_list: [
+          ...state.todos_list.filter((element) => element.id != id),
+          {id, task, isDone},
+        ],
       };
     }
     default:
