9836f2b3d6af944be135055e27a2c757828f7d48
Royce Chua <royce236@gmail.com>
2020-10-12 23:37:15 +0800

    Add Redux Persist Configuration

--- App.js
+++ App.js
@@ -16,63 +16,66 @@ import AddTodoScreen from './src/screens/AddTodoScreen';
 import EditTodoScreen from './src/screens/EditTodoScreen';
 
 import { Provider } from 'react-redux'
-import store from './src/redux/store'
+import configureStore from './src/redux/configureStore';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            initialParams={{ todo: "" }}
-            options={{
-            title: 'TODO App',
-            headerStyle: {
-              backgroundColor: '#2196F3',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}
-            
-          />
-          <Stack.Screen
-            name="AddTodo"
-            component={AddTodoScreen}
-            options={{
-            title: 'Add Todo',
-            headerBackTitle: '',
-            headerStyle: {
-              backgroundColor: '#2196F3',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}
-          />
-          <Stack.Screen
-            name="EditTodo"
-            component={EditTodoScreen}
-            options={{
-            title: 'Edit Todo',
-            headerBackTitle: '',
-            headerStyle: {
-              backgroundColor: '#2196F3',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <Provider store={configureStore().store}>
+      <PersistGate loading={null} persistor={configureStore().persistor}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              initialParams={{ todo: "" }}
+              options={{
+              title: 'TODO App',
+              headerStyle: {
+                backgroundColor: '#2196F3',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+              
+            />
+            <Stack.Screen
+              name="AddTodo"
+              component={AddTodoScreen}
+              options={{
+              title: 'Add Todo',
+              headerBackTitle: '',
+              headerStyle: {
+                backgroundColor: '#2196F3',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+            />
+            <Stack.Screen
+              name="EditTodo"
+              component={EditTodoScreen}
+              options={{
+              title: 'Edit Todo',
+              headerBackTitle: '',
+              headerStyle: {
+                backgroundColor: '#2196F3',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
